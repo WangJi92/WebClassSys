079ff6751d969fa0ceefa3904d849b7f1f27aaef
汪吉 <983433479@qq.com>
2016-10-07 16:01:35 +0800

    [modify] 主要是为了基础的ConstantParam 这个是当前应用程序中的统一配置文件

---
+++ WebClassSys/src/main/java/com/hdu/cms/common/ConstantParam/ConstantParam.java
@@ -0,0 +1,24 @@
+package com.hdu.cms.common.ConstantParam;
+
+import org.omg.PortableInterceptor.INACTIVE;
+
+import java.io.Serializable;
+
+/**
+ * Created by JetWang on 2016/10/6.
+ */
+public class ConstantParam  implements Serializable{
+    /**
+     * 默认的分页的大小
+     */
+    public static Integer DEFAULT_PAGE_SIZE = 15;
+
+    /**
+     * 默认分页的No
+     */
+    public static Integer DEFAULT_PAGE_NO = 1;
+
+
+
+
+}
