8d3e1ce3b6f01e818b8339f93cb8300b9e93fb17
汪吉 <983433479@qq.com>
2016-10-10 19:46:58 +0800

    [add] 工具类 List to dotString

---
+++ WebClassSys/src/main/java/com/hdu/cms/common/Utils/ArrayUtils.java
@@ -0,0 +1,43 @@
+package com.hdu.cms.common.Utils;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by JetWang on 2016/10/10.
+ */
+public class ArrayUtils {
+
+    /**
+     * [ 1, 2] list 转为 1,2,3,4,5
+     * @param ids
+     * @return
+     */
+    public static  String listToDotString(List<Integer> ids){
+        if(CollectionUtils.isNotEmpty(ids)){
+            return ids.toString().replace("[","").replace("]"," ").trim();
+        }
+        return " ";
+    }
+
+    /**
+     * 1,2,3,4 转化为 [1,2,3,4]
+     * @param dotStr
+     * @return
+     */
+    public static List<Integer> StringDotToList(String dotStr){
+        if(StringUtils.isNotBlank(dotStr)){
+            String[] arry = dotStr.split(",");
+            List<Integer> list = Lists.newArrayList();
+            for(String item :arry){
+                list.add(Integer.valueOf(item));
+            }
+            return list;
+        }
+        return Lists.newArrayList();
+    }
+}
