9e354b179e6acede089816415dd35995688f45e0
汪吉 <983433479@qq.com>
2016-11-14 13:41:09 +0800

    [add]数据字典中的！这种写法也是必须控制啊，不然没法继续写下去了。。。。。。。

--- WebClassSys/src/main/java/com/hdu/cms/common/ConstantParam/DICTIONARY.java
+++ WebClassSys/src/main/java/com/hdu/cms/common/ConstantParam/DICTIONARY.java
@@ -7,21 +7,23 @@ import org.apache.commons.lang3.StringUtils;
  * 数据字典中查找信息哦
  */
 public enum DICTIONARY {
-    USERINFO("DIC_USER"),//用户分类
-    CLASSROOM("DIC_CLASSROOM"),//教室分类
-    CLASSSTATE("DIC_CLASSSTATE"),//教室状态分类
-    REPAIRCLASSFIY("DIC_REPAIRCLASSFIY"),//维修类别
-    URGENCYREPAIR("DIC_URGENCYREPAIR"),//紧急报修，不同报修
-    BUIDING("DIC_BUDING"),//楼宇
-    EQUIPMENT("DIC_EQUIPMENT")//设备信息
+    USERINFO("DIC_USER",1,"用户分类"),//用户分类
+    CLASSROOM("DIC_CLASSROOM",2,"教室分类"),//
+    CLASSSTATE("DIC_CLASSSTATE",4,"教室状态分类"),//
+    REPAIRCLASSFIY("DIC_REPAIRCLASSFIY",8,"维修类别"),//
+    URGENCYREPAIR("DIC_URGENCYREPAIR",16,"紧急程度"),//，不同报修
+    BUIDING("DIC_BUDING",32,"楼宇类型"),//楼宇
+    EQUIPMENT("DIC_EQUIPMENT",64,"设备信息")
     ;
-    DICTIONARY(String value) {
-        this.value = value;
+    DICTIONARY(String value,Integer key,String name) {
+        this.value = value;// type
+        this.key=key;//key
+        this.name=name;// name
     }
-    public static DICTIONARY getDictionary(String name){
-        if(StringUtils.isNotEmpty(name)){
+    public static DICTIONARY getDictionary(String value){
+        if(StringUtils.isNotEmpty(value)){
             for(DICTIONARY item :DICTIONARY.values()){
-                if(item.getValue().equals(name)){
+                if(item.getValue().equals(value)){
                     return item;
                 }
             }
@@ -29,6 +31,52 @@ public enum DICTIONARY {
         //最笨的方法
         return DICTIONARY.USERINFO;
     }
+    public static DICTIONARY getDictionaryByKey(Integer key){
+        if(key != null){
+            for(DICTIONARY item :DICTIONARY.values()){
+                if(item.getKey() == key){
+                    return item;
+                }
+            }
+        }
+        //最笨的方法
+        return DICTIONARY.USERINFO;
+    }
+    public static DICTIONARY getDictionaryByName(String  name){
+        if(StringUtils.isNotEmpty(name)){
+            for(DICTIONARY item :DICTIONARY.values()){
+                if(item.getKey().equals(name)){
+                    return item;
+                }
+            }
+        }
+        //最笨的方法
+        return DICTIONARY.USERINFO;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    private String name;
+    private Integer key;
     private String value;
     public String getValue(){
         return this.value;
