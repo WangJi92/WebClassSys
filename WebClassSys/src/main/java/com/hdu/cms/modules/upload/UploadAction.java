ee11626774abf116d42bec963d94f7dd2faef044
汪吉 <983433479@qq.com>
2016-11-14 12:51:57 +0800

    [add] 上传图片信息的模块，这个非常好理解的

---
+++ WebClassSys/src/main/java/com/hdu/cms/modules/upload/UploadAction.java
@@ -0,0 +1,56 @@
+package com.hdu.cms.modules.upload;
+
+import com.hdu.cms.common.BaseAction.BaseAction;
+import com.hdu.cms.common.ConstantParam.ConstantParam;
+import com.hdu.cms.common.ConstantParam.UNIQUETYPE;
+import com.hdu.cms.common.Utils.ActionResult;
+import com.hdu.cms.common.Utils.RandomUtil;
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by JetWang on 2016/10/17.
+ */
+@Controller
+@RequestMapping("/test")
+public class UploadAction extends BaseAction{
+
+    /**
+     * 返回在message中的是图片在当前项目中保存图片的名称
+     * @param file
+     * @return
+     */
+    @RequestMapping("/upload")
+    @ResponseBody
+    public ActionResult getMultiFile(@RequestParam("file")List<MultipartFile> file){
+        ActionResult result = new ActionResult(true);
+        if(CollectionUtils.isNotEmpty(file)){
+            for(MultipartFile Item: file){
+                String orginName = Item.getOriginalFilename();
+                String orldName =orginName.substring(orginName.indexOf("."));
+                String newName = RandomUtil.getUniqueNubmerString(20, UNIQUETYPE.LOWERALPHABET)+orldName;
+                String path = getServletContext().getRealPath("/");
+                path+=ConstantParam.DEFAULT_IMAGE_PATH;
+                File newFile = new File(path+newName);
+                try {
+                    Item.transferTo(newFile);
+                    result.setMessage(newName);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    result.setMessage("error");
+                    result.setSuccess(false);
+                }
+
+            }
+        }
+     return result;
+    }
+
+}
