9a368363d9e057e5524ec47d2117b677f64b2fca
汪吉 <983433479@qq.com>
2016-11-17 13:12:46 +0800

    [add]自习教室查询~~这里面的信息进行处理，主要是调用了timetable表中的信息和外键教室的信息

---
+++ WebClassSys/src/main/webapp/user/selfstudyclassroom/js/slefstudyclassroom.js
@@ -0,0 +1,110 @@
+/**
+ * Created by JetWang on 2016/11/17.
+ */
+
+define(basePath + "/user/selfstudyclassroom/js/slefstudyclassroom",
+    [
+        basePath + "/views/dictionary/service/dictionaryService",
+        basePath + "/libs/bootstrap-table/dist/bootstrap-table",
+        basePath + "/libs/bootstrap-table/dist/bootstrap-table-locale-all"
+    ],
+    function (require, exports, module) {
+        var $dicService = require(basePath + "/views/dictionary/service/dictionaryService");
+        var $timetable;
+        var timetableoptions = {
+            striped: true,  //表格显示条纹
+            pagination: true, //启动分页
+            pageSize: 10,  //每页显示的记录数
+            pageNumber: 1, //当前第几页
+            pageList: [10, 15, 20, 25],  //记录数可选列表
+            search: false,  //是否启用查询
+            showColumns: false,  //显示下拉框勾选要显示的列
+            showRefresh: false,  //显示刷新按钮
+            // toolbar:"#toolbar_timetable",//设置旁边的框框的属性
+            sidePagination: "server", //表示服务端请求
+            //设置为undefined可以获取pageNumber，pageSize，searchText，sortName，sortOrder
+            //设置为limit可以获取limit, offset, search, sort, order
+            url: basePath + "/timeTableAction/pageInfo",
+            responseHandler: function (res) {
+                //远程数据加载之前,处理程序响应数据格式,对象包含的参数: 我们可以对返回的数据格式进行处理
+                //在ajax后我们可以在这里进行一些事件的处理
+                return res.data;
+            },
+            queryParamsType: "undefined",
+            queryParams: function queryParams(params) {   //设置查询参数
+                var param = {
+                    //这里是在ajax发送请求的时候设置一些参数
+                    pageNo: params.pageNumber,
+                    pageSize: params.pageSize,
+                    whichWeek: $("*[name='whichWeek']").attr("value"),
+                    whichDay: $("*[name='whichDay']").attr("value"),
+                    whichLesson: $("*[name='whichLesson']").attr("value"),
+                    timetableType:$("*[name='timetableType']").attr("value"),
+                    classRoomName:$("*[name='classRoomName']").attr("value")
+                };
+                return param;
+            },
+            onLoadSuccess: function (data) {  //加载成功时执行
+
+            },
+            onLoadError: function () {  //加载失败时执行
+
+            },
+            columns: [
+                {
+                    title: '教室名称',
+                    field: 'classRoomName',
+                    align: 'center',
+                    valign: 'middle'
+                },
+                {
+                    title: '课程时间',
+                    field: 'lessontime',
+                    align: 'center',
+                    valign: 'middle',
+                    formatter: lessontimeFormatter
+                },
+                {
+                    field: 'classTypeName',
+                    title: '教室内型',
+                    align: 'center'
+                }, {
+                    field: 'seatNo',
+                    title: '座位数',
+                    align: 'center'
+                },
+                {
+                    field: 'typeStr',
+                    title: '使用状态',
+                    align: 'center'
+                }
+            ]
+        };
+
+        function lessontimeFormatter(value, row, index) {
+            return "【"+row.weekStr+"*"+row.dayStr+"*"+row.lessonStr+"】";
+        }
+        function initEvent() {
+            $timetable = $("#timetable").bootstrapTable(timetableoptions);
+            $(".col-sm-12").on("click", ".timetable_btn_search", function () {
+                $timetable.bootstrapTable("refresh");
+            });
+            $dicService.getDicSelectByName("DIC_CLASSSTATE", function (result) {
+                if (result.success == true && result.data != null) {
+                    $.each(result.data, function (index, value) {
+                        $("select[name='timetableType']").append("<option value='" + value.value + "'>" + value.key + "</option>");
+
+                    });
+                }
+            });
+            $("#search_calssname").keydown(function(e){
+                if(e.keyCode ==13){//触发键盘事件enter 防止冒泡产生
+                    $timetable.bootstrapTable("refresh");
+                    return false;
+                }
+            });//触发enter事件，重新搜索表格信息
+        }
+        module.exports = {
+            initEvent: initEvent
+        }
+    });
---
+++ WebClassSys/src/main/webapp/user/selfstudyclassroom/slefstudyclassroom.jsp
@@ -0,0 +1,103 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: JetWang
+  Date: 2016/11/17
+  Time: 12:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ include file="../../common/common.jsp" %>
+<script src="<%=baseUrl%>/libs/bootstrap-table/dist/bootstrap-table.js" type="text/javascript"></script>
+<script src="<%=baseUrl%>/libs/bootstrap-table/dist/bootstrap-table-locale-all.js" type="text/javascript">
+</script>
+<link rel="stylesheet" href="<%=baseUrl%>/libs/bootstrap-table/dist/bootstrap-table.css" type="text/css">
+<%--模板的头--%>
+<%@ include file="./../common/header.jsp" %>
+<%--模板的left--%>
+<%@ include file="./../common/left.jsp" %>
+<div class="maincontent row">
+  <ul class="breadcrumb">
+    <li class="active">自习教室查询</li>
+  </ul>
+  <div class="col-sm-12">
+    <div class="row">
+      <div class="col-sm-12">
+        <form class="form-inline" style="margin-bottom: 10px" role="form">
+          <div class="form-group">
+            <label class="control-label">周次:</label>
+            <select name="whichWeek" class="form-control">
+              <option value="-1">全部</option>
+              <option value="1">第1周</option>
+              <option value="2">第2周</option>
+              <option value="3">第3周</option>
+              <option value="4">第4周</option>
+              <option value="5">第5周</option>
+              <option value="6">第6周</option>
+              <option value="7">第7周</option>
+              <option value="8">第8周</option>
+              <option value="9">第9周</option>
+              <option value="10">第10周</option>
+              <option value="11">第11周</option>
+              <option value="12">第12周</option>
+              <option value="13">第13周</option>
+              <option value="14">第14周</option>
+              <option value="15">第15周</option>
+              <option value="16">第16周</option>
+              <option value="17">第17周</option>
+              <option value="18">第18周</option>
+              <option value="19">第19周</option>
+              <option value="20">第20周</option>
+            </select>
+          </div>
+          &nbsp;&nbsp;&nbsp;
+          <div class="form-group">
+            <label class="control-label">星期:</label>
+            <select name="whichDay" class="form-control">
+              <option value="-1">全部</option>
+              <option value="1">星期一</option>
+              <option value="2">星期二</option>
+              <option value="3">星期三</option>
+              <option value="4">星期四</option>
+              <option value="5">星期五</option>
+              <option value="6">星期六</option>
+              <option value="7">星期天</option>
+            </select>
+          </div>
+          &nbsp;&nbsp;&nbsp;
+          <div class="form-group">
+            <label class="control-label">课程时间:</label>
+            <select name="whichLesson" class="form-control">
+              <option value="-1">全部</option>
+              <option value="1">第一二节</option>
+              <option value="2">第三四五节</option>
+              <option value="4">第六七节</option>
+              <option value="8">第八九节</option>
+              <option value="16">第十到十二节</option>
+            </select>
+          </div>
+          &nbsp;&nbsp;&nbsp;
+          <div class="form-group">
+            <label class="control-label">状态:</label>
+            <select name="timetableType" class="form-control">
+              <option value="-1">全部</option>
+            </select>
+          </div>
+          &nbsp;&nbsp;&nbsp;
+          <div class="form-group">
+            <input class="form-control" id="search_calssname" name="classRoomName" title="座位数大于..." placeholder="教室名称" >
+          </div>
+          &nbsp;&nbsp;&nbsp;
+          <button type="button" class="btn btn-default btn-primary timetable_btn_search">搜索</button>
+        </form>
+      </div>
+    </div>
+    <table id="timetable">
+    </table>
+  </div>
+</div>
+<%@ include file="./../common/footter.jsp" %>
+<script>
+  seajs.use(basePath + "/user/selfstudyclassroom/js/slefstudyclassroom", function (data) {
+    data.initEvent();
+  });//这里是调用初始化函数，进行初始化工作
+</script>
