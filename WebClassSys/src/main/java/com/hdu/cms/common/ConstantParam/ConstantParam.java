8f4850dcf6cb671abd3c85d5278f667d21975099
汪吉 <983433479@qq.com>
2016-11-21 00:11:10 +0800

    [modify】 增加了一些常量，包括导出EXCEL的名字，和我们Session Cookie的key  的名字，然后进行加盐操作的处理！保证数据的安全性，不被查看

--- WebClassSys/src/main/java/com/hdu/cms/common/ConstantParam/ConstantParam.java
+++ WebClassSys/src/main/java/com/hdu/cms/common/ConstantParam/ConstantParam.java
@@ -12,13 +12,38 @@ public class ConstantParam  implements Serializable{
      * 默认的分页的大小
      */
     public static Integer DEFAULT_PAGE_SIZE = 15;
-
-    /**
-     * 默认分页的No
-     */
     public static Integer DEFAULT_PAGE_NO = 1;
 
+    //图片的保存路径
+    public  static String DEFAULT_IMAGE_PATH ="image\\";
 
+    /**
+     * excel 导出目录
+     */
+    public static  String DEFAULT_EXCEL_PATH ="excel\\";
+
+    public static String  EXPORT_USER ="user.xls";
+
+    public static  String EXPORT_EQUIPMENT="equipment.xls";
+
+    public static String EXPORT_DIC = "dictionary.xls";
+
+    public static String EXPORT_MAINTAIN ="maintain.xls";
+
+    public static String EXPORT_APPLYCLASSOOM="applyclassroom.xls";
+
+    public static String DEFAULT_IMAGE_TYPE =".jpg";
+    /**
+     * 保存在serssion 中的登录信息 通过base64进行加密然后在加盐操作
+     */
+    public  static  String SESSION_USERNAME ="CMS_USER";
+    public  static  String  COOKIE_NAME ="COOKIE_NAME_CMS";
+    public static String COOKIE_PASSWORD="COOKIE_VALUE_CMS";
+    public  static String ADD_SALT="SALT_I_LOVE_HANGDIAN";
+    /**
+     * 默认初始化的星期的数量
+     */
+    public  static Integer DEFAULT_MAX_WEEK = 20;
 
 
 }
